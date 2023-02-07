d7ee94184440407172b50241eba1d04e1fa46186
SixEcho84 <53911359+SixEcho84@users.noreply.github.com>
2023-02-07 17:03:33 +0100

    Create check_openrc.sh

---
+++ check_openrc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+crashed=$(rc-status --crashed)
+
+if [ -n "$crashed" ]; then
+        echo "CRITICAL: crashed services: ${crashed//$'\n'/, }"
+        exit 2
+else
+        echo 'OK: no services crashed'
+fi
